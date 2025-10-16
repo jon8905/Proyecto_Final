6596bf3192e5441d39673f7454e234926e4a3b7d
Jon8905 <8905ajon@gmail.com>
2025-10-16 17:55:42 -0500

    Subiendo el proyecto inicial a la rama Smeller

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-//IMPORTAR APP
 const app = require('./src/app');
+const PORT = process.env.PORT || 3000;
 
-//CARGAR VARIABLES DE ENTORNO
-require('dotenv').config();
-
-//DEFINIMOS EL PUERTO PARA EJECUTAR EL PROYECTO
-const PORT = process.env.PORT || 3000
-
-//INICIAMOS EL PROYECTO Y DEMOSTRAMOS QUE SE ESTA EJECUTANDO CON UN MENSAJE
-app.listen(PORT,() => {
-    console.log(`servidor escuchando desde http://localhots:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+
+// Aquí se agregan las rutas
+// app.use('/api/usuarios', require('./routes/usuarios.routes'));
+
+module.exports = app;
